787ea4ce261225ca60d5a187c98e95e49489181b
prarthana-maheta <142563768+prarthana-maheta@users.noreply.github.com>
2024-05-17 19:56:40 +0530

    Add files via upload

---
+++ may_15.py
@@ -0,0 +1,23 @@
+# int a = 12
+
+a = "12"
+b=1234
+c=12.45
+
+# print(type(c))
+
+# a,b= '1',2.4567
+
+# a,b = b,a
+
+print(f" {a} 45689pjhgvnmdf, ccmfx mfnmyfcm,fm., {b} , {c}")
+# write a python code to find the averge of 5 numbers 
+
+# f-string formatted string 
+
+# 10,23,4,567 & 45 average of this numbers is avg
+
+a=12
+print(type(a))
+b=str(a)
+print(type(b))
---
+++ may_17.py
@@ -0,0 +1,27 @@
+# name=input("enter your name")
+# print(type(name))
+# print(f"my name is {name}")
+# num1=int(input("enter num1"))
+# print(num1+2)
+
+
+# intergers ---non iterable 1230
+# float --- 1.234
+# char--string iterable--"12345678"
+# complex --- 1j+1
+
+# mutable, immuatble
+# ordered , unordered
+
+str1="1234567890wertyuiozxcvbnm!@#$%^&*(    234567890)"
+# immuatble, ordered data type 
+# 0,1,2,3............
+# ..................,-3-2,-1
+# Index 
+# positive, negative
+print(str1[5])
+print(str1[-5])
+
+# [start:stop:strp/direction]
+print(str1[10:15:1])
+print(str1[::-2])
